24860981da56fb04eeea092923544c44f15b606b
gabriel-casari <gabriel.casari@utp.ac.pa>
2024-01-30 17:04:49 -0500

    Reconfiguración de tailwind. Archivos nuevos. cerrado.html y exito.html listos.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        offwhite: "#F6F6F6",
+        white: "#FFFFFF",
+        black: "#101010",
+        red: "#FF151F",
+        blue: "#1D2269",
+        sky: "#0596FF",
+      },
+      fontFamily: {
+        'kumbh_regular': ['kumbh_regular','sans-serif'],
+        'kumbh_bold': ['kumbh_bold','sans-serif']
+      },
+    },
   },
   plugins: [],
 };
