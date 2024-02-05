60bd169624c624d65e85baaffdeabb77e79d5dbb
gabriel-casari <gabriel.casari@utp.ac.pa>
2024-02-05 09:47:59 -0500

    Seguimiento del frontend

--- static/js/toggle-idioma-dropdown.js
+++ static/js/toggle-idioma-dropdown.js
@@ -1,5 +1,5 @@
-/* Este js / función permite hacer el toggle para mostrar o no mostrar las opciones de idiomas en el dropdown */ 
+/* Esta función permite hacer el toggle para mostrar los idiomas disponibles en el dropdown */
 function toggleIdiomaDropdown() {
-    let IdiomaDropdown = document.querySelector('#dropdownIdiomas');
-    IdiomaDropdown.classList.toggle('hidden');
+  let IdiomaDropdown = document.querySelector("#dropdownIdiomas");
+  IdiomaDropdown.classList.toggle("hidden");
 }
