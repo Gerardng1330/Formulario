78d091efd802f50521b5eb937f99ff17785369ff
meowsterdeveloper <felxansl@gmail.com>
2024-02-05 16:26:52 -0500

    unstable merge

--- registro/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from .views import UserRegistrationView, UserValidationView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('validate/<str:token>/', UserValidationView.as_view(), name='user-validation'),
-]
