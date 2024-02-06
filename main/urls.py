11d6c1fca87e60a4fc96e3e520c65b1ad6be78b7
gerar <gerar132000ng@gmail.com>
2024-02-06 16:15:16 -0500

    postfix : traduccion

--- main/urls.py
+++ main/urls.py
@@ -20,9 +20,8 @@ urlpatterns += i18n_patterns(
     path('register/', include('registro.urls'), name='registro'),
     path('', include('formularios.urls'), name='formularios'),
     
-    path('', include('formularios.urls')),
-    path('en/', include('formularios.urls')),
-    path('es/', include('formularios.urls')),
+   
+   
 )
 if settings.DEBUG:
     #No hacer esto en producción. Esto es solo para el manejo local de archivos estáticos.
