237da3afd6c61700a75658958414feb96b1f4234
gabriel-casari <gabriel.casari@utp.ac.pa>
2024-02-19 17:17:07 -0500

    style: unificación de nombres de scripts JavaScript y validación en tiempo real en formulario.html

--- frontend/static/js/form_input_validation.js
+++ frontend/static/js/form_input_validation.js
@@ -1,15 +1,19 @@
 /* Referencias de todos los inputs del form */
 const form = document.getElementById("form");
-const inputs = form.querySelectorAll("input"); /* Todos los inputs del form */
 const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
+const genero = document.getElementById("genero");
+const nacionalidad = document.getElementById("nacionalidad");
 const fecha_nacimiento = document.getElementById("fecha_nacimiento");
 const alergia = document.getElementById("alergia");
 const id_file = document.getElementById("id_file");
 const cv_file = document.getElementById("cv_file");
+const codigo1 = document.getElementById("codigo1");
 const Telefono1 = document.getElementById("Telefono1");
+const codigo3 = document.getElementById("codigo3");
 const telefono_emergencia = document.getElementById("telefono_emergencia");
+const codigo2 = document.getElementById("codigo2");
 const Telefono2 = document.getElementById("Telefono2");
 const nombre_contacto = document.getElementById("nombre_contacto");
 const direccion_principal = document.getElementById("direccion_principal");
@@ -17,84 +21,128 @@ const direccion_secundaria = document.getElementById("direccion_secundaria");
 const ciudad = document.getElementById("ciudad");
 const Estado_Provincia = document.getElementById("Estado_Provincia");
 const codigo_postal = document.getElementById("codigo_postal");
+const Cargo1 = document.getElementById("Cargo1");
+const Cargo2 = document.getElementById("Cargo2");
+const turno = document.getElementById("turno");
+const nivel_ingles = document.getElementById("nivel_ingles");
 const fecha_inicio = document.getElementById("fecha_inicio");
+const Transporte = document.getElementById("Transporte");
+const Conociste = document.getElementById("Conociste");
 const referencia = document.getElementById("referencia");
+
+/* Inicialización de expresiones regulares */
+const letrasRegex = /^[a-zA-Z]+$/;
+const numerosRegex = /^[0-9]+$/;
+const nombreApellidoRegex = /^[A-Z][a-z]*$/;
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const espacioRegex = /^\s+$/;
 
 /* Previene que el form se envíe antes de validar los inputs.
 Se ejecuta cuando se da click al botón APLICAR AHORA */
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validarInputs();
-});
-
-/* Permite la validación a tiempo real.
-Se ejecuta cuando cualquier valor de cualquier input dentro del form, cambia. */
-form.addEventListener("input", (e) => {
-  validarInputs();
+  validarNombre();
+  validarApellido();
+  validarEmail();
+  validarGenero();
 });
 
 /* Función que recibe un elemento html (el input) y un mensaje a mostrar (que se trae del html como variable, para permitir la internacionalización */
-const setError = (element, message) => {
-  const input_control = element.parentElement; /* Referencia de la div padre del input proporcionado */
-  const message_display = input_control.querySelector(".message"); /* Referencia al span donde va el mensaje */
-  message_display.innerText = message; /* Le asignamos el mensaje a mostrar en el span */
-  message_display.classList.add("text-red"); /* Color de texto rojo para denotar error */
+function setError(element, message) {
+  const input_control = element.parentElement; /* Referencia: div padre del input proporcionado */
+  const message_display = input_control.querySelector(".message"); /* Referencia: span del mensaje */
+  message_display.classList.remove("hidden");
+  message_display.innerText = message; /* Asignación: mensaje a mostrar en el span */
+  message_display.classList.add("text-red");
   message_display.classList.remove("text-lime-500");
-  message_display.classList.remove("hidden"); /* Le quitamos la clase hidden para mostrar el mensaje */
-};
+  element.classList.add("border-red"); /* Estilización del input */
+  element.classList.remove("border-gray-300");
+  element.classList.remove("border");
+  element.classList.add("border-2");
+  element.classList.remove("border-lime-500");
+}
 
 /* Función que recibe un elemento html (el input) y un mensaje a mostrar (que se trae del html como variable, para permitir la internacionalización */
-const setSuccess = (element, message) => {
-  const input_control = element.parentElement; /* Referencia de la div padre del input proporcionado */
-  const message_display = input_control.querySelector(".message"); /* Referencia al span donde va el mensaje */
-  message_display.innerText = message; /* Le asignamos el mensaje a mostrar en el span */
-  message_display.classList.add("text-lime-500"); /* Color de texto verde para denotar éxito */
-  message_display.classList.remove("text-red");
-  message_display.classList.remove("hidden"); /* Le quitamos la clase hidden para mostrar el mensaje */
-};
+function setSuccess(element, message) {
+  const input_control = element.parentElement; /* Referencia: div padre del input proporcionado */
+  const message_display = input_control.querySelector(".message"); /* Referencia: span del mensaje */
+  message_display.classList.add("hidden");
+  element.classList.add("border-lime-500"); /* Estilización del input */
+  element.classList.remove("border-gray-300");
+  element.classList.remove("border");
+  element.classList.add("border-2");
+  element.classList.remove("border-red");
+}
 
-/* Función para validar los inputs */
-const validarInputs = () => {
-  /* Obtenemos los valores de todos los inputs */
-  const nombre_value = nombre.value.trim();
-  const apellido_value = apellido.value.trim();
-  const email_value = email.value.trim();
-  const fecha_nacimiento_value = fecha_nacimiento.value.trim();
-  const alergia_value = alergia.value.trim();
-  const id_file_value = id_file.value.trim();
-  const cv_file_value = cv_file.value.trim();
-  const Telefono1_value = Telefono1.value.trim();
-  const telefono_emergencia_value = telefono_emergencia.value.trim();
-  const Telefono2_value = Telefono2.value.trim();
-  const nombre_contacto_value = nombre_contacto.value.trim();
-  const direccion_principal_value = direccion_principal.value.trim();
-  const direccion_secundaria_value = direccion_secundaria.value.trim();
-  const ciudad_value = ciudad.value.trim();
-  const Estado_Provincia_value = Estado_Provincia.value.trim();
-  const codigo_postal_value = codigo_postal.value.trim();
-  const fecha_inicio_value = fecha_inicio.value.trim();
-  const referencia_value = referencia.value.trim();
+/* Validar nombre */
+nombre.addEventListener("input", function validarNombre() {
+  const input = nombre;
+  const value = nombre.value.trim();
 
-  /* ############ Validaciones ############ */
-  /* Validación de cada campo required */
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.trim() === "") {
-      setError(inputs[i], "No puede dejar esta campo vacío");
-      inputs[i].classList.add("border-red");
-      inputs[i].classList.remove("border-gray-300");
-      inputs[i].classList.remove("border-lime-500");
-    }
+  if (value === "") {
+    /* Campo requerido */
+    setError(input, m_campo_requerido);
+  } else if (!nombreApellidoRegex.test(value)) {
+    /* Inicia con mayúscula? */
+    setError(input, m_nombre_formato);
+  } else if (espacioRegex.test(value)) {
+    /* Escribió más de un nombre? */
+    setError(input, m_nombre_formato);
+  } else if (value.length < 2 || value.length > 20) {
+    /* Longitud mínima y máxima */
+    setError(input, m_longitud_min_max);
+  } else {
+    setSuccess(input, "");
   }
+});
 
-  /*   if (nombre_value === "" || apellido_value === "" || email_value === "" || fecha_nacimiento_value === "" || id_file_value === "" || cv_file_value === "" || Telefono1_value === "" || telefono_emergencia_value === "" || Telefono2_value === "" || nombre_contacto_value === "" || direccion_principal_value === "" || direccion_secundaria_value === "" || ciudad_value === "" || Estado_Provincia_value === "" || codigo_postal_value === "" || fecha_inicio_value === "") {
-    setError(nombre, "Por favor, rellene el campo");
-    nombre.classList.add("border-red");
-    nombre.classList.remove("border-gray-300");
-    nombre.classList.remove("border-lime-500");
-  }  else {
-    setSuccess(nombre, "Nombre correcto");
-    nombre.classList.add("border-lime-500");
-    nombre.classList.remove("border-gray-300");
-    nombre.classList.remove("border-red");
-  } */
-};
+/* Validar apellido */
+nombre.addEventListener("input", function validarApellido() {
+  const input = apellido;
+  const value = apellido.value.trim();
+
+  if (value === "") {
+    /* Campo requerido */
+    setError(input, m_campo_requerido);
+  } else if (!nombreApellidoRegex.test(value)) {
+    /* Inicia con mayúscula? */
+    setError(input, m_apellido_formato);
+  } else if (espacioRegex.test(value)) {
+    /* Escribió más de un apellido? */
+    setError(input, m_apellido_formato);
+  } else if (value.length < 2 || value.length > 20) {
+    /* Longitud mínima y máxima */
+    setError(input, m_longitud_min_max);
+  } else {
+    setSuccess(input, "");
+  }
+});
+
+/* Validar email */
+email.addEventListener("input", function validarEmail() {
+  const input = email;
+  const value = email.value.trim();
+
+  if (value === "") {
+    /* Campo requerido */
+    setError(input, m_campo_requerido);
+  } else if (value.length < 2 || value.length > 30 || !emailRegex.test(value)) {
+    /* Longitud mínima y máxima - Formato ejemplo@email.com */
+    setError(input, m_email_formato);
+  } else {
+    setSuccess(input, "");
+  }
+});
+
+/* Validar género */
+genero.addEventListener("click", function validarGenero() {
+  const input = genero;
+  const value = genero.value.trim();
+
+  if (value === "") {
+    /* Campo requerido */
+    setError(input, m_campo_requerido);
+  } else {
+    setSuccess(input, "");
+  }
+});
