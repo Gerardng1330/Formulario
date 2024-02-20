12691276e78d31c3cd75ee0a128edb7fd6670d47
Nanu <manuelnunez2629@gmail.com>
2024-02-20 16:47:52 -0500

    fix: email_backend

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -124,7 +124,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom user model
 
 #Correo al mandar
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'backend.main.email_backend.EmailBackend'
 EMAIL_HOST = 'mail.arvcloud.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
