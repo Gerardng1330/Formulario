99ea5e547d7870ec988a3f9c7537fbbc51ac3857
gabriel-casari <gabriel.casari@utp.ac.pa>
2024-03-19 17:05:34 -0500

    feat: Continuación del desarrollo del frontend de registro
    Se crearon referencias en el archivo registro_validation.js para manejar la validación del formulario.
    
    chore: Actualización del package.json y requirements.txt

---
+++ static/js/registro_validation.js
@@ -0,0 +1,11 @@
+/* Referencias */
+const registro_form = document.getElementById("registro_form");
+const Nombre = document.getElementById("Nombre");
+const Apellido = document.getElementById("Apellido");
+const Usuario = document.getElementById("Usuario");
+const Correo = document.getElementById("Correo");
+const Contraseña = document.getElementById("Contraseña");
+const Contraseña1 = document.getElementById("Contraseña1");
+const indicador1 = document.getElementById("indicador1");
+const indicador2 = document.getElementById("indicador2");
+const indicador3 = document.getElementById("indicador3");
